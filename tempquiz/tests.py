9320f5ac246a211b2c985fec6ca549c91fc92bf7
Lee Azzarello <lee.azzarello@astraspace.com>
2023-07-07 00:40:53 -0700

    fix tests

--- tempquiz/tests.py
+++ tempquiz/tests.py
@@ -1,22 +1,25 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from django.urls import reverse
 
-class HomepageTests(SimpleTestCase):
+from .models import Temperature
+
+class TemperatureTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.temp = Temperature.objects.create(time="2023-07-07 00:00:00+00:00",
+                                              temperature=0.0)
+
+    def test_model_content(self):
+        self.assertEqual(self.temp.time, "2023-07-07 00:00:00+00:00")
+
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(self):
+    def test_homepage(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "home.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("home"))
-        self.assertContains(response, "<h1>Homepage</h1>")
 
 class AboutpageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
