a69cfd04da95942df49fc92b79faf2a6df85fa5f
Lee Azzarello <lee.azzarello@astraspace.com>
2023-07-07 15:03:47 -0700

    query graphql

--- temperature/schema.py
+++ temperature/schema.py
@@ -6,21 +6,12 @@ from tempquiz.models import Temperature
 class TemperatureType(DjangoObjectType):
     class Meta:
         model = Temperature
-        fields = ("time", "temperature")
+        fields = ("id", "time", "temperature")
 
 class Query(graphene.ObjectType):
-    current_temperature = graphene.List(TemperatureType)
-    min_max_temperature = graphene.List(TemperatureType)
+    temperatures = graphene.List(TemperatureType)
 
-    def resolve_current_temperature(root, info):
-        return Temperature.objects.select_related("temperature").last()
-    
-    def resolve_min_max_temperature(root, info, time):
-        try:
-            before = Temperature.objects.get(time="2020-12-07T12:00:00+00:00")
-            after = Temperature.objects.get(time="2020-12-06T12:00:00+00:00")
-            return []
-        except Temperature.DoesNotExist:
-            return None
+    def resolve_temperatures(root, info):
+        return Temperature.objects.all()
 
 schema = graphene.Schema(query=Query)
--- temperature/settings.py
+++ temperature/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "tempquiz",
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+GRAPHENE = {
+    "SCHEMA": "temperature.schema.schema"
+}
