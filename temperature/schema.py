83cee7c032f1e9ba21c34dd1bc5c9f8a90d17371
Lee Azzarello <lee.azzarello@astraspace.com>
2023-07-07 20:09:05 -0700

    interesting results

--- temperature/schema.py
+++ temperature/schema.py
@@ -1,21 +1,26 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models import Max, Min
 
 from tempquiz.models import Temperature
 
 class TemperatureType(DjangoObjectType):
     class Meta:
         model = Temperature
-        fields = ("id", "timestamp", "value")
+        fields = ("id", "timestamp", "value", "max", "min")
 
 class Query(graphene.ObjectType):
     current_temperature = graphene.Field(TemperatureType)
-    temperature_statistics = graphene.List(TemperatureType)
+    temperature_statistics = graphene.Field(TemperatureType)
 
     def resolve_current_temperature(root, info):
         return Temperature.objects.last()
 
-    def resolve_temperature_statistics(root, info, **kwargs):
-        return Temperature.objects.all()
+    def resolve_temperature_statistics(root, info):
+        max = Temperature.objects.all().aggregate(Max('value'))['value__max']
+        min = Temperature.objects.all().aggregate(Min('value'))['value__min']
+        # probably need to filter on value__max and value__min
+        # using the graphene python library and django-filter
+        return [max, min]
 
 schema = graphene.Schema(query=Query)
--- temperature/settings.py
+++ temperature/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "tempquiz",
-    'graphene_django',
+    "graphene_django",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
